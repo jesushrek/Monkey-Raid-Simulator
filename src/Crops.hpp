4d93c41ca260b888b36f9a5c6dbdd872634c60df
Shrek68 <Sandesh1234poudels@gmail.com>
2026-01-07 20:47:03 +0545

    done

--- src/Crops.hpp
+++ src/Crops.hpp
@@ -10,7 +10,7 @@ class Crops:public Creatures
 { 
    public:
         Crops(int x, int y, int id)
-            :Creatures (Point{ x, y }, Creatures::Crops, id, 'c', 10, 0)
+            :Creatures (Point{ x, y }, Creatures::Crops, id, 'c', 4, 0)
             {}
         virtual void Ai(Field<Config::HEIGHT, Config::WIDTH>& field) override;
 };
