563f3e7b70c28c398203522d2abe6d8edcdd9cfa
Shrek68 <Sandesh1234poudels@gmail.com>
2026-01-09 18:01:15 +0545

    add more files

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -1,15 +1,15 @@
 #ifndef COSNTANTS_HPP
 #define COSNTANTS_HPP
 
-//#define CROPS
+#define CROPS
 namespace Config
 { 
     constexpr int HEIGHT = 50;
     constexpr int WIDTH = 100;
     constexpr int HUMANCOUNT = 1;
     constexpr int SPEED = 90;
-    constexpr int MONKEYCOUNT = 0;
-    constexpr int HOUSECOUNT = 0;
-    constexpr int DOGCOUNT = 1;
+    constexpr int MONKEYCOUNT = 1;
+    constexpr int HOUSECOUNT = 8;
+    constexpr int DOGCOUNT = 5;
 };
 #endif
